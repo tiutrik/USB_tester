f894d21bf51e33c89e3247c642c237bb9be334a4
Short Circuit <tiutrik2019@gmail.com>
2025-03-04 22:56:32 +0200

    Update main.ino
    battery charge indicator added

--- main.ino
+++ main.ino
@@ -6,8 +6,9 @@
 #define DATA_PLUS_PIN A1
 #define DATA_MINUS_PIN A2
 #define VCC_PIN A3
+#define BATTERY_PIN A3  
 
-
+// Настройки дисплея
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1); 
@@ -17,16 +18,17 @@ void setup() {
   pinMode(DATA_PLUS_PIN, INPUT);
   pinMode(DATA_MINUS_PIN, INPUT);
   pinMode(VCC_PIN, INPUT);
+  pinMode(BATTERY_PIN, INPUT);
 
   Serial.begin(9600);
 
-
+  // Инициализация дисплея
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {  
     Serial.println(F("Не удалось найти дисплей SSD1306"));
     while (true);
   }
 
-  
+  // Приветственный экран
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
@@ -36,14 +38,31 @@ void setup() {
   display.setCursor(20, 20);
   display.print("SHORT CIRCUIT");
   display.display();
-  delay(2500);  
+  delay(2000);  
+
   display.clearDisplay();
+}
+
+void drawBatteryIndicator(float voltage) {
+  int level = map(voltage * 100, 300, 420, 0, 5); 
+  level = constrain(level, 0, 5);  
+
+  int x = 110, y = 2, w = 15, h = 7;  
+  display.drawRect(x, y, w, h, SSD1306_WHITE); 
+  display.fillRect(x + w, y + 2, 2, h - 4, SSD1306_WHITE); 
+
+  for (int i = 0; i < level; i++) {
+    display.fillRect(x + 2 + (i * 3), y + 2, 2, h - 4, SSD1306_WHITE); 
+  }
 }
 
 void loop() {
   int dataPlus = analogRead(DATA_PLUS_PIN);
   int dataMinus = analogRead(DATA_MINUS_PIN);
   int vcc = analogRead(VCC_PIN);
+  
+  float batteryVoltage = analogRead(BATTERY_PIN) * (5.0 / 1023.0);
+
 
   display.clearDisplay();
 
@@ -52,7 +71,7 @@ void loop() {
   display.print("DATA+ ");
   if (dataPlus < 50) {  
     display.print("X");
-    tone(BUZZER_PIN, 1000, 200);  
+    tone(BUZZER_PIN, 1000, 200);
   } else {
     display.print(dataPlus);
   }
@@ -75,12 +94,16 @@ void loop() {
     display.print(vcc);
   }
 
- 
-  display.setCursor(90, 5);
+  
+  display.setCursor(86, 3);
   display.print("USB");
-  display.setCursor(83, 20);
+   display.setCursor(80, 20);
   display.print("TESTER");
 
+  
+  drawBatteryIndicator(batteryVoltage);
+
   display.display();
-  delay(500);  
+  delay(500);
 }
+
